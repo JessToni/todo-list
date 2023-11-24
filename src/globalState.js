80c3f0abcdf0c2a273327f4299ed144699c6fbff
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-11-24 11:27:08 +0800

    Add default "My Day" project on page load

--- src/globalState.js
+++ src/globalState.js
@@ -1,1 +1,6 @@
+import { Project } from "./project";
+
 export const projects = [];
+
+const myDay = new Project("My Day");
+projects.push(myDay);
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -2,11 +2,13 @@ import { projects } from "./globalState";
 import createTodoHeader from './todo-header';
 import createTodoContentContainer from './todo-content';
 import createMyProjects from './project-modal';
+import { displayProjects } from "./projectDisplay";
 
 function initialLoad() {
     createTodoHeader();
     createTodoContentContainer();
     createMyProjects();
+    displayProjects();
 }
 
 export default initialLoad;
