a94e4ff757cd6ded937cd6fd4a1867855e68d1cd
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-11-26 09:38:31 +0800

    Make displayTasks() display project tasks
    Still needs add task div for every project

--- src/globalState.js
+++ src/globalState.js
@@ -1,6 +1,16 @@
 import { Project } from "./project";
+import { Task } from "./task";
 
 export const projects = [];
 
 const myDay = new Project("My Day");
 projects.push(myDay);
+
+//Create test project
+const testProject = new Project("Test Project");
+
+//Add test tasks to testProject
+testProject.tasks.push(new Task("Jog", "Run around the oval", "2023-01-05", "High"));
+testProject.tasks.push(new Task("Frolic", "Walk around happily", "2023-01-06", "Medium"));
+
+projects.push(testProject);
--- src/projectDisplay.js
+++ src/projectDisplay.js
@@ -19,16 +19,42 @@ export const displayProjects = () => {
     });
 }
 
-function displayTasks(obj) {
-    alert("Hanokio");
-}
-/*function displayTasks(project) {
+function displayTasks(project) {
     const rightContainer = document.querySelector('.right-container');
-    rightContainer.innerHTML = "";
+    rightContainer.innerHTML = ''; // Clear existing content
 
     project.tasks.forEach(task => {
+        //Task container
         const taskElement = document.createElement('div');
-        taskElement.textContent = task.name;
+        taskElement.classList.add('task');
+        
+        //Task name
+        const taskName = document.createElement('div');
+        taskName.textContent = task.name;
+        taskName.classList.add('task-name');
+
+        //Task description
+        const taskDescription = document.createElement('div');
+        taskDescription.textContent = task.description;
+        taskDescription.classList.add('task-description');
+
+        //Task due date
+        const taskDueDate = document.createElement('div');
+        taskDueDate.textContent = `Due: ${task.dueDate}`;
+        taskDueDate.classList.add('task-due-date');
+
+        // Task priority
+        const taskPriority = document.createElement('div');
+        taskPriority.textContent = `Priority: ${task.priority}`;
+        taskPriority.classList.add('task-priority');
+
+        //Append all properties to taskElement
+        taskElement.appendChild(taskName);
+        taskElement.appendChild(taskDescription);
+        taskElement.appendChild(taskDueDate);
+        taskElement.appendChild(taskPriority);
+
+        //Append all taskElement to rightContainer
         rightContainer.appendChild(taskElement);
     });
-}*/
+}
