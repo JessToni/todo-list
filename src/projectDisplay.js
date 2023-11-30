4ea3b8b60ec78a4c6a0fb7d54e97e9afba16e81f
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-11-30 18:30:30 +0800

    Fill out showTaskForm function
    Clicking add task now shows form that asks for task info
    
    Clicking submit now creates new task
    
    Add task div needs to be hidden on click
    
    Cancel button on form must also be added

--- src/projectDisplay.js
+++ src/projectDisplay.js
@@ -1,4 +1,5 @@
 import { projects } from "./globalState";
+import { Task } from "./task";
 
 export const displayProjects = () => {
     const projectList = document.querySelector('.project-container');
@@ -23,6 +24,17 @@ function displayTasks(project) {
     const rightContainer = document.querySelector('.right-container');
     rightContainer.innerHTML = ''; // Clear existing content
 
+    //Create and append the Add Task Div
+    const addTaskDiv = document.createElement('div');
+    addTaskDiv.textContent = 'Add Task';
+    addTaskDiv.classList.add('add-task');
+    addTaskDiv.addEventListener('click', () => {
+        //Function to display the task form
+        showTaskForm(project, addTaskDiv);
+    });
+    rightContainer.appendChild(addTaskDiv);
+
+    //Display existing tasks
     project.tasks.forEach(task => {
         //Task container
         const taskElement = document.createElement('div');
@@ -57,4 +69,74 @@ function displayTasks(project) {
         //Append all taskElement to rightContainer
         rightContainer.appendChild(taskElement);
     });
+
+    function showTaskForm(project, addTaskDiv) {
+        const rightContainer = document.querySelector('.right-container')
+        
+        //Create form
+        const form = document.createElement('form');
+        form.id = 'task-form';
+        
+        //Task name input
+        const nameInput = document.createElement('input');
+        nameInput.type = 'text';
+        nameInput.placeholder = 'Task Name';
+        nameInput.required = true;
+        form.appendChild(nameInput);
+
+        //Task description input
+        const descriptionInput = document.createElement('textarea');
+        descriptionInput.placeholder = 'Task Description';
+        descriptionInput.required = true; //required field
+        form.appendChild(descriptionInput);
+
+        //Task due date input
+        const dueDateInput = document.createElement('input');
+        dueDateInput.type = 'date';
+        dueDateInput.required = true; //required field
+        form.appendChild(dueDateInput);
+
+        //Priority Checkboxes
+        const priorities = ["High", "Medium", "Low"];
+        const priorityContainer = document.createElement('div');
+
+        priorities.forEach(priority => {
+            const label = document.createElement('label');
+            const checkbox = document.createElement('input');
+            checkbox.type = 'radio';
+            checkbox.name = 'priority';
+            checkbox.value = priority;
+
+            label.appendChild(checkbox);
+            label.appendChild(document.createTextNode(priority));
+            priorityContainer.appendChild(label);
+        });
+
+        form.appendChild(priorityContainer)
+
+        const submitButton = document.createElement('button');
+        submitButton.type = 'submit';
+        submitButton.textContent = 'Add Task';
+        form.appendChild(submitButton);
+
+        //Add form just before the 'Add Task' div
+        rightContainer.insertBefore(form, addTaskDiv);
+
+        //Form submission event
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            //Create a new task and add it to the project
+            const newTask = new Task(
+                nameInput.value,
+                descriptionInput.value,
+                dueDateInput.value,
+                form.priority.value //assuming priority is set via radio buttons
+            );
+            project.tasks.unshift(newTask); //adds the new task at the beginning
+
+            //Redisplay tasks
+            displayTasks(project);
+        });
+    }
 }
